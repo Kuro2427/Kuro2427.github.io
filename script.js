8833cea936dd17bb15b82309cc9c43760fd57fd3
Kuro2427 <53385574+Kuro2427@users.noreply.github.com>
2023-01-04 21:12:54 -0300

    website update momento

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let nameplates=['assets/name1.png','assets/name2.png','assets/name3.png','assets/name4.png'];
-let captions=['random caption goes here','this website goes so hard, feel free to bookmark','aaaaa','we kinning','awww the scringly...','home of swag','also try trickmint.gay','also try elisttm.space','nominated for Forbes \"fucking website ever 2003\" award','never trust a bitch who\'s seen lain','jet set ratio download free no virus punjabi','fuck wordpress all my homies hate wordpress','css more like cs ass :trollface:','hi skedgy if you\'re reading this i love you!!!!','yea it\'s a perfect blue reference','best viewed on desktop','\"lily, this is a legitimate thread, please keep it on-topic /srs\"','i miss flash','mobile more like mogetoffmywebsiteitisbestviewedondesktop'];
+let captions=['random caption goes here','this website goes so hard, feel free to bookmark','aaaaa','we kinning','awww the scringly...',
+'home of swag','also try trickmint.gay','also try elisttm.space','nominated for Forbes \"fucking website ever 2003\" award',
+'never trust a bitch who\'s seen lain','jet set ratio download free no virus punjabi','fuck wordpress all my homies hate wordpress',
+'css more like cs ass :trollface:','hi skedgy if you\'re reading this i love you!!!!','yea it\'s a perfect blue reference','best viewed on desktop',
+'\"lily, this is a legitimate thread, please keep it on-topic /srs\"','i miss flash','mobile more like mogetoffmywebsiteitisbestviewedondesktop'];
 
 var randomnum = Math.floor(Math.random() * captions.length);
 finalcaption = captions[randomnum];
@@ -28,7 +32,7 @@ function refreshCap(){
   div.textContent = finalcaption;
 
   //:3
-  var fun=Math.floor(Math.random() * 50);
+  var fun=Math.floor(Math.random() * 420);
 
   if (fun==0){
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
